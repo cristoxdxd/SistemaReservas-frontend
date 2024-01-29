fe0d09b6de0a8443f3bb3b818b80844c2a705a16
Cristopher Herrera <cristopher.herrera@epn.edu.ec>
2024-01-29 09:58:34 -0500

    fix: image import

--- src/constants/habitaciones.ts
+++ src/constants/habitaciones.ts
@@ -1,8 +1,8 @@
-const image1 = 'src/assets/8714bd8f-aacf-4b86-b965-ae6f7bda7bf6.jpg'
-const image2 = 'src/assets/ca2426ac-1c99-41b2-a85c-3e025074c3bc.jpg'
-const image3 = 'src/assets/e9bb97de-dc28-41aa-b5b6-ded33d4fb3ea.jpg'
-const image4 = 'src/assets/4fa49748-a03c-4ac5-a7b3-4294be9a5b89.jpg'
-const image5 = 'src/assets/17dc1ad0-75a7-4e7e-a7ce-c878940eb1fb.jpg'
+import image1 from '../assets/8714bd8f-aacf-4b86-b965-ae6f7bda7bf6.jpg'
+import image2 from '../assets/ca2426ac-1c99-41b2-a85c-3e025074c3bc.jpg'
+import image3 from '../assets/e9bb97de-dc28-41aa-b5b6-ded33d4fb3ea.jpg'
+import image4 from '../assets/4fa49748-a03c-4ac5-a7b3-4294be9a5b89.jpg'
+import image5 from '../assets/17dc1ad0-75a7-4e7e-a7ce-c878940eb1fb.jpg'
 
 export interface IHabitacion {
     id: string;
@@ -10,7 +10,7 @@ export interface IHabitacion {
     description: string;
     price: number;
     capacity: number;
-    image: string;
+    image: any;
 }
 
 export const Habitacion: IHabitacion[] = [
