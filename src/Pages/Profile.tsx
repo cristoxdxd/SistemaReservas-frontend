0dd3695ac8deffc0d4bb97fa11e6403c03a8355f
Paola Aucapiña <102838577+PaolaMaribel18@users.noreply.github.com>
2024-02-06 19:36:26 -0500

    feat: update Profile component

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -3,15 +3,20 @@ import React from 'react';
 interface ProfileProps {
     username: string;
     email: string;
-    // Otros datos del perfil
+    age: number;
+    location: string;
 }
 
-const Profile: React.FC<ProfileProps> = ({ username, email }) => {
+const Profile: React.FC<ProfileProps> = ({ username, email, age, location }) => {
+    
+    
     return (
-        <div style={{ backgroundColor: 'white' }}>
+        <div className="bg-white p-4 rounded-md shadow-md">
             <h1 className="text-2xl font-bold mb-4 text-blue-500">Mi Perfil</h1>
-            <h1>Perfil de {username}</h1>
-            <p>Email: {email}</p>
+            <h1 className="text-xl mb-2">Perfil de {username}</h1>
+            <p className="text-gray-700">Email: {email}</p>
+            <p className="text-gray-700">Edad: {age} años</p>
+            <p className="text-gray-700">Ubicación: {location}</p>
             {/* Otros detalles del perfil */}
         </div>
     );
