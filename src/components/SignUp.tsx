f5ff690b6da2ae80af851d8e5eb6bbb70ce64221
Ericksp2001 <erick.perez01@epn.edu.ec>
2024-03-03 17:24:21 -0500

    fix: :zap: bug correction in login and sign up.
    the visual bug correction was implemented, which required double-clicking and prevented the proper functioning of the login.

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -4,6 +4,7 @@ import { auth } from "../Firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useEffect } from "react";
 import SnowFlakeLogo from "../assets/snowflake_nav.svg";
+import { IAppState, useAppStore } from "../Storage";
 
 
 interface SignUpProps {
@@ -13,6 +14,7 @@ interface SignUpProps {
 
 export const SignUp: React.FC<SignUpProps> = ({ onSuccess, onFailure }) => {
   const registerForm = useForm({ mode: "onBlur" });
+  const { loading, setLoading } = useAppStore((state: IAppState) => state);
 
   const onSubmit = () => {
     registerForm.trigger();
@@ -27,6 +29,7 @@ export const SignUp: React.FC<SignUpProps> = ({ onSuccess, onFailure }) => {
       )
         .then((response) => {
           console.log("response", response);
+          setLoading(false);
           onSuccess();
         })
         .catch((error) => {
@@ -44,168 +47,80 @@ export const SignUp: React.FC<SignUpProps> = ({ onSuccess, onFailure }) => {
 
   return (
     <>
-      {/* <div className="flex flex-col items-center justify-center animate-fade-down">
-        <div className="flex flex-col items-center bg-gray-800 rounded-lg shadow-lg p-8 w-80">
-          <h1 className="text-2xl font-bold mb-4 text-white">Register</h1>
-          <input
-            {...registerForm.register("firstName", {
-              required: "Este campo es requerido.",
-              pattern: {
-                value: /^[A-Za-z]+$/i,
-                message: "Porfavor solo ingresa letras.",
-              },
-            })}
-            className="w-full h-10 text-lg mb-4 px-2 py-1 border border-gray-300 rounded text-black"
-            type="text"
-            placeholder="Ingrese Nombre"
-          />
-          {!isEmpty(registerForm.formState.errors) && (
-            <p className="text-red-500 mb-4">
-              {
-                get(
-                  registerForm,
-                  "formState.errors.firstName.message",
-                  ""
-                ) as string
-              }
-            </p>
-          )}
-          <input
-            {...registerForm.register("email", {
-              required: "Este campo es requerido.",
-              pattern: {
-                value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i,
-                message: "Porfavor ingresa un correo valido.",
-              },
-            })}
-            className="w-full h-10 text-lg mb-4 px-2 py-1 border border-gray-300 rounded text-black"
-            type="email"
-            placeholder="Ingrese correo electronico"
-          />
-          {!isEmpty(registerForm.formState.errors) && (
-            <p className="text-red-500 mb-4">
-              {
-                get(
-                  registerForm,
-                  "formState.errors.email.message",
-                  ""
-                ) as string
-              }
-            </p>
-          )}
-          <input
-            {...registerForm.register("password", {
-              required: "Este campo es requerido.",
-              minLength: {
-                value: 8,
-                message: "La contrasenia debe tener al menos 8 caracteres.",
-              },
-            })}
-            className="w-full h-10 text-lg mb-4 px-2 py-1 border border-gray-300 rounded text-black"
-            type="password"
-            placeholder="Ingrese contrase&ntilde;a"
-          />
-          {!isEmpty(registerForm.formState.errors) && (
-            <p className="text-red-500 mb-4">
-              {
-                get(
-                  registerForm,
-                  "formState.errors.password.message",
-                  ""
-                ) as string
-              }
-            </p>
-          )}
-          <div className="flex justify-center">
-            <button
-              className="px-4 py-2 mt-4 bg-blue-800 text-gray-400 rounded hover:bg-blue-600"
-              onClick={onSubmit}
-            >
-              Register
-            </button>
-          </div>
-        </div>
-      </div> */}
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img
-            className="w-16 h-16 mr-2"  // Ajuste del tamaño del logo
-            src={SnowFlakeLogo}
-            alt="SnowFlake"
-          />
+        <a href="#" className="flex items-center mb-6 text-xl font-semibold text-white dark:text-black">
+          <img className="w-12 h-12 md:w-16 md:h-16 mr-2" src={SnowFlakeLogo} alt="SnowFlake" />
+          Hotel Copo de Nieve
         </a>
-
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
+              Create an account
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form onSubmit={registerForm.handleSubmit(onSubmit)} className="space-y-4 md:space-y-6" action="#">
               <div>
-                <label htmlFor="firstName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
-                <input{...registerForm.register("firstName", {
-                  required: "Este campo es requerido.",
-                  pattern: {
-                    value: /^[A-Za-z]+$/i,
-                    message: "Porfavor solo ingresa letras.",
-                  },
-                })} type="text" name="firstName" id="firstName" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your name" />
-                {!isEmpty(registerForm.formState.errors) && (
+                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
+                <input
+                  {...registerForm.register("name", {
+                    required: "Este campo es requerido.",
+                  })}
+                  className={`w-full h-10 text-lg mb-4 px-2 py-1 border border-gray-300 rounded text-black ${!isEmpty(registerForm.formState.errors.name) ? "border-red-500" : ""
+                    }`}
+                  type="text"
+                  placeholder="Your name"
+                />
+                {!isEmpty(registerForm.formState.errors.name) && (
                   <p className="text-red-500 mb-4">
-                    {
-                      get(
-                        registerForm,
-                        "formState.errors.email.message",
-                        ""
-                      ) as string
-                    }
+                    {get(registerForm, "formState.errors.name.message", "") as string}
                   </p>
                 )}
               </div>
               <div>
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                <input{...registerForm.register("email", {
-                  required: "Este campo es requerido.",
-                  pattern: {
-                    value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i,
-                    message: "Porfavor ingresa un correo valido.",
-                  },
-                })} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" />
-                {!isEmpty(registerForm.formState.errors) && (
+                <input
+                  {...registerForm.register("email", {
+                    required: "Este campo es requerido.",
+                    pattern: {
+                      value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i,
+                      message: "Ingrese un correo válido",
+                    },
+                  })}
+                  className={`w-full h-10 text-lg mb-4 px-2 py-1 border border-gray-300 rounded text-black ${!isEmpty(registerForm.formState.errors.email) ? "border-red-500" : ""
+                    }`}
+                  type="text"
+                  placeholder="name@company.com"
+                />
+                {!isEmpty(registerForm.formState.errors.email) && (
                   <p className="text-red-500 mb-4">
-                    {
-                      get(
-                        registerForm,
-                        "formState.errors.email.message",
-                        ""
-                      ) as string
-                    }
+                    {get(registerForm, "formState.errors.email.message", "") as string}
                   </p>
                 )}
               </div>
               <div>
                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                <input {...registerForm.register("password", {
-                  required: "Este campo es requerido.",
-                  minLength: {
-                    value: 8,
-                    message: "La contrasenia debe tener al menos 8 caracteres.",
-                  },
-                })} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                {!isEmpty(registerForm.formState.errors) && (
+                <input
+                  {...registerForm.register("password", {
+                    required: "Este campo es requerido.",
+                  })}
+                  className={`w-full h-10 text-lg mb-4 px-2 py-1 border border-gray-300 rounded text-black ${!isEmpty(registerForm.formState.errors.password) ? "border-red-500" : ""
+                    }`}
+                  type="password"
+                  placeholder="••••••••"
+                />
+                {!isEmpty(registerForm.formState.errors.password) && (
                   <p className="text-red-500 mb-4">
-                    {
-                      get(
-                        registerForm,
-                        "formState.errors.password.message",
-                        ""
-                      ) as string
-                    }
+                    {get(registerForm, "formState.errors.password.message", "") as string}
                   </p>
                 )}
               </div>
-              <button onClick={onSubmit} type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+              <button
+                type="submit"
+                className={`w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 ${!registerForm.formState.isValid ? "cursor-not-allowed opacity-50" : ""
+                  }`}
+                disabled={!registerForm.formState.isValid}
+              >
+                {loading ? "Creating account..." : "Registrarse"}
+              </button>
             </form>
           </div>
         </div>
