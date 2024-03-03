7e4466a2bf9b08624e721bd29122849cbeb00230
Redjean <jeanasencio2000@gmail.com>
2024-03-03 16:14:23 -0500

    feat: Graphic improvements and corrections.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: "https://service-booking-auu3.onrender.com",
         secure: false,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
@@ -15,4 +15,3 @@ export default defineConfig({
     },
   },
 });
-//https://service-booking-auu3.onrender.com
