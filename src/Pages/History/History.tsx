9756f174a3505f1996c715c845760ad2a81e7a5f
Ericksp2001 <erick.perez01@epn.edu.ec>
2024-03-04 17:52:47 -0500

    fix: :fire: eliminacion de la barra de busqueda
    errores de implementacion y preferencia en el apoyo de la interfaz grafica

--- src/Pages/History/History.tsx
+++ src/Pages/History/History.tsx
@@ -1,7 +1,6 @@
 import { NavBar } from "../../components/NavBar";
 import { HistoryContainer } from "../../components/HistoryContainer/HistoryContainer";
 import { useHistory } from "./State/useHistory";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { useEffect, useState } from "react";
 import { Footer } from "../../components/Footer";
 import { Link } from "react-router-dom";
@@ -13,7 +12,6 @@ export const History = () => {
   const [isListAvailabilityFetched, setIsListAvailabilyFetched] = useState(false);
   const [isListBookingFetched, setIsListBookingFetched] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const availabilityTimeout = setTimeout(() => {
@@ -43,22 +41,6 @@ export const History = () => {
     return () => clearTimeout(loadingTimeout);
   }, []);
 
-  // Función para buscar
-  const search = (term: string) => {
-    return listBooking.filter(booking =>
-      booking._id.toLowerCase().includes(term.toLowerCase())
-    );
-  };
-
-  const handleSearchTermChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value);
-  };
-
-  const handleSearch = () => {
-    const results = search(searchTerm);
-    console.log("Resultados de la búsqueda:", results);
-    // Puedes hacer algo con los resultados aquí, como mostrarlos en otro componente
-  };
 
 
   return (
@@ -66,29 +48,6 @@ export const History = () => {
     <>
       <NavBar />
       <br />
-      <form onSubmit={handleSearch} className="max-w-md mx-auto">
-      <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-      <div className="relative">
-        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-          <MagnifyingGlassIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" />
-        </div>
-        <input
-          type="search"
-          id="default-search"
-          value={searchTerm}
-          onChange={handleSearchTermChange}
-          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search ..."
-          required
-        />
-        <button
-          type="submit"
-          className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Search
-        </button>
-      </div>
-    </form>
       <br />
       {(!isLoading && isListAvailabilityFetched && isListBookingFetched) ? (
         <>
